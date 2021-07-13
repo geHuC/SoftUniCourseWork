eeb67a1dfeaf901b8cbe88e99f95bccc51a49a28
geHuC <79841526+geHuC@users.noreply.github.com>
2021-07-13 16:37:25 +0300

    Completed exercise 5

--- JavaScript - Applications/03.Remote-Data-and-Authentication/05.Fisher-Game/app.js
+++ JavaScript - Applications/03.Remote-Data-and-Authentication/05.Fisher-Game/app.js
@@ -1,27 +1,203 @@
+//Using the old html template where login and home are two seperate files(same one as in the excercise)
+
+//Get Div Elements
 const catchesDiv = document.querySelector('#catches');
 const loadButton = document.querySelector('main aside .load');
+const addButton = document.querySelector("#addForm .add");
 
+//Attach Event Listeners
+loadButton.addEventListener('click', loadFishes);
+addButton.addEventListener('click', addNewFish);
+catchesDiv.addEventListener('click', catchButtonHandler);
 
-loadButton.addEventListener('click', loadFishes)
-clearCatches();
+//Call startup functions
+clearCatches(); 
 ifLoggedIn();
 
 function ifLoggedIn(){
-    if(!localStorage.getItem('token')){
+    // probably should also check for expired sessions as if we restart the server this would still say we are logged in but our token will be obsolete
+    if(!localStorage.getItem('token')){ 
         return;
     }
+    //If logged in change login to log out and enable the add button
     const navigationDiv = document.querySelector('#guest');
     let a = navigationDiv.querySelector('a');
     a.textContent = 'Log out!';
     a.href = '#';
     a.addEventListener('click', logOutHandler);
+    addButton.disabled = false;
 }
 
+//Event delegation for the catches
+function catchButtonHandler(e){
+    if(e.target.type !== 'submit'){ // if what we clicked is not button ignore
+        return;
+    }
+    if(e.target.classList.contains('delete')){
+        deleteFish(e);
+    }
+    if(e.target.classList.contains('update')){
+        updateFish(e);
+    }
+}
+async function deleteFish(e){
+    const fishId = e.target.parentElement.dataset.id;
+    const url = `http://localhost:3030/data/catches/${fishId}`;
+    try{
+        let response = await fetch(url,{
+            method: 'DELETE',
+            headers: {
+                'Content-Type' : 'application/json',
+                'X-Authorization': localStorage.getItem('token')
+            },
+        });
+        if(!response){
+            throw new Error('Somenthing went wrong!');
+        }
+        e.target.parentElement.remove(); // remove element form the DOM probably should not be done here
+    } catch (e) {
+        console.log(e);
+    }
+}
+async function updateFish(e){
+    const fishId = e.target.parentElement.dataset.id;
+    const url = `http://localhost:3030/data/catches/${fishId}`;
+    let catchObject = validateAndCreateCatchObject(e); // checking if input is valid
+    if(!catchObject) return; // if catch is empty stop here
+
+    try{
+        let response = await fetch(url,{
+            method: 'PUT',
+            headers: {
+                'Content-Type' : 'application/json',
+                'X-Authorization': localStorage.getItem('token')
+            },
+            body: JSON.stringify(catchObject)
+        });
+        if(!response){
+            throw new Error('Somenthing went wrong!');
+        }
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+function addNewFish(e){
+    //Refactoring mess had all of validateAndCreateCatchObject here but need it agian for update so...
+    let catchObject = validateAndCreateCatchObject(e);
+    if(!catchObject) return ;// if catch is empty stop here
+        addFishToServer(catchObject);
+    //Clear the input from the fieldset
+    e.target.parentElement.querySelectorAll('input').forEach(x => x.value = '');
+}
+function validateAndCreateCatchObject(e){
+        //Didn't want to chage the html to form so had to get everything manually (would've needed the elements anyway for error checking purposes)
+        const formFiledset = e.target.parentElement;
+        const angler = formFiledset.querySelector('.angler');
+        const weight = formFiledset.querySelector('.weight');
+        const species = formFiledset.querySelector('.species');
+        const location = formFiledset.querySelector('.location');
+        const bait = formFiledset.querySelector('.bait');
+        const captureTime = formFiledset.querySelector('.captureTime');
+        //Clearing any previous error displays
+
+        clearErrorStyling(formFiledset); //Since I tried to make things pretty I need a little bit of cleanup
+
+        //Checking validity of input information should probably be refactored in another function as well
+        let inputIsValid = true;
+        if(!validateTextValue(angler)) inputIsValid = false; 
+        if(!validateTextValue(species)) inputIsValid = false; 
+        if(!validateTextValue(location)) inputIsValid = false; 
+        if(!validateTextValue(bait)) inputIsValid = false; 
+        if(!validateNumberValue(weight)) inputIsValid = false; 
+        if(!validateIntValue(captureTime)) inputIsValid = false; 
+        if(!inputIsValid) return false; // if any input isn't valid return 
+        
+        return catchObject = {
+            angler: angler.value,
+            weight: Number(weight.value),
+            species: species.value,
+            location: location.value,
+            bait: bait.value,
+            captureTime: Number(captureTime.value)
+        }
+}
+async function addFishToServer(fishCatch){
+    const url = 'http://localhost:3030/data/catches';
+    try{
+        let response = await fetch(url,{
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json',
+                'X-Authorization': localStorage.getItem('token')
+            },
+            body: JSON.stringify(fishCatch)
+        })
+        if(!response.ok){
+            throw new Error('Something went wrong');
+        }
+        let jsonResponse = await response.json(); 
+        // this should not be here but can't be bothered to refactor it right now
+        appendCatchesToDOM([jsonResponse]); //function expect an array  
+    } catch (e) {
+        console.log(e);
+    }
+}
+function clearErrorStyling(container){  
+    container.querySelectorAll('.error-message').forEach(x => x.remove()); // remove all error messages
+    container.querySelectorAll('.error').forEach(x => x.classList.remove('error')); // take off the error class
+}
+
+// Validation fuctions
+function validateTextValue(element){
+    let value = element.value;
+    if(value.trim() === ''){
+        createErrorMessage('Must not be empty',element);
+        return false;
+    }
+    if(!isNaN(value)){
+        createErrorMessage('Must be text',element);
+        return false;
+    }
+    return true;
+}
+function validateNumberValue(element){
+    let value = element.value;
+    if(value.trim() === ''){
+        createErrorMessage('Must not be empty',element);
+        return false;
+    }
+    if(isNaN(value)){
+        createErrorMessage('Must be number',element);
+        return false;
+    }
+    return true;
+}
+function validateIntValue(element){ // time needs to be an integer so needed another one
+    let value = element.value;
+    if(!validateNumberValue(element)){ // check first if it is a number then if it is integer
+        return false;
+    }
+    if(!Number.isInteger(Number(value))){ 
+        createErrorMessage('Must be integer',element);
+        return false;
+    }
+    return true;
+}
+// Helper function that attaches error messages to the invalid input and adds a class to color the borders
+function createErrorMessage(message,element){
+    let small = document.createElement('small');
+    small.textContent = message;
+    small.classList.add('error-message');
+    element.classList.add('error');
+    element.after(small);
+}
+
+
 async function logOutHandler(e){
-    e.preventDefault();
+    e.preventDefault(); // Since it is an anchor tag we need to prevent redirection when clicked
     const url = 'http://localhost:3030/users/logout';
     try{
-
         let response = await fetch(url, {
             method: 'GET',
             headers: {
@@ -37,20 +213,20 @@ async function logOutHandler(e){
 }
 
 
-
 async function loadFishes(){
     const url = 'http://localhost:3030/data/catches/';
     let response = await fetch(url);
     let jsonResponse = await response.json();
-    appendCatchesToDOM(jsonResponse);
+    clearCatches(); // clear old data in the DOM
+    appendCatchesToDOM(jsonResponse); //append the pulled data
 }
 
 function appendCatchesToDOM(catches){
     let fragment = document.createDocumentFragment();
     catches.forEach(c => fragment.appendChild(createCatchCard(c)));
-    clearCatches();
     catchesDiv.appendChild(fragment);
 }
+
 function createCatchCard(data){
     let catchDiv = createElement('div','','catch');
 
@@ -82,7 +258,8 @@ function createCatchCard(data){
     catchDiv.dataset.ownerId = data._ownerId;
     return catchDiv;
 }
-function clearCatches(){
+
+function clearCatches(){ // simple cleanup function to delete every catch from the container
     catchesDiv.querySelectorAll('.catch').forEach(x => x.remove());
 }
 //Function that creates Inputs of with various different properties
